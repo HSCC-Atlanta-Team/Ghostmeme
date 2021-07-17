95780a33bd9c00dfa5d6dd0089b6fd972dbab7f6
saleemsaafir <saleemsaafir@gmail.com>
2021-07-17 14:30:34 +0000

    Added lazy load js files and started Ajax test 1

---
+++ app/ajax_test1.php
@@ -0,0 +1,3 @@
+<?php require 'init.php' ?>
+
+<img class="lazy block" data-src="https://images.unsplash.com/photo-1555234432-e681ac3ad0ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=753&q=80"/>
